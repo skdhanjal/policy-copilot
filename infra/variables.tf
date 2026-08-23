03b2ecba5db307bdde3813c1479d25d71a69070b
Shashi Kumar <skdhanjal777@gmail.com>
2026-08-23 14:41:18 +0000

    Phase 8: store all secrets in Secret Manager (DB password, API keys, gateway keys)

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,8 @@ variable "region" {
 variable "db_password" {
   sensitive = true
 }
+
+variable "openai_api_key" { sensitive = true }
+variable "gemini_api_key" { sensitive = true }
+variable "litellm_master_key" { sensitive = true }
+variable "gateway_app_key" { sensitive = true }
